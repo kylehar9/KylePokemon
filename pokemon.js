514e44810353614f5bd7472eed9031af8a27b5c7
Kyle <kylehar9@gmail.com>
2020-01-22 14:40:19 -0700

    All files added

---
+++ pokemon.js
@@ -0,0 +1,105 @@
+/*
+    Description of each position
+    1st = Pokemon Name
+    2nd = Type of Pokemon
+    3rd = Image path for Pokemon
+    4th = Attack Move Name
+    5th = Attack Move Type
+    6th = Attack Move Power
+    7th = Defend Move Name
+    8th = Defend Move Type
+    9th = Defend Move Power
+*/
+
+let pokemonArray = [
+    [
+        "Bulbasaur",
+        "Grass",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        "Tackle",
+        "Normal",
+        5,
+        "Razor Leaf",
+        "Grass",
+        15
+    ],
+    [
+        "Charmander",
+        "Fire",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+        "Scratch",
+        "Normal",
+        7,
+        "Ember",
+        "Fire",
+        15
+    ],
+    [
+        "Squirtle",
+        "Water",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+        "Scratch",
+        "Normal",
+        7,
+        "Water Gun",
+        "Water",
+        15
+    ],
+    [
+        "Pidgey",
+         "Flying",
+         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+         "Tackle",
+         "Normal",
+         5,
+         "Gust",
+         "Flying",
+         12
+    ],
+    [
+        "Rattata",
+        "Normal",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png",
+        "Tackle",
+        "Normal",
+        5,
+        "Bite",
+        "Normal",
+        15
+    ],
+    [
+        "Spearow",
+        "Flying",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/21.png",
+        "Tackle",
+        "Normal",
+        5,
+        "Peck",
+        "Flying",
+        12
+    ],
+    [
+        "Ekans",
+        "Poison",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png",
+        "Wrap",
+        "Normal",
+        5,
+        "Poison Sting",
+        "Poison",
+        12
+    ],
+    [
+        "Pikachu",
+        "Electric",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+        "Quick Attack",
+        "Normal",
+        8,
+        "Thundershock",
+        "Electric",
+        15
+    ]
+]
+
+
